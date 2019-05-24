48234fbdf043e6486860d6fd726bb1119eaee172
engineersonal <37497432+engineersonal@users.noreply.github.com>
2019-05-24 13:34:05 +0530

    Second commit

--- js/main.js
+++ js/main.js
@@ -1,5 +1,26 @@
 const fetchUsers = async () => {
-    await (await fetch('/.netlify/functions/getusers')).json();
+    // await (await fetch('/.netlify/functions/getusers')).json();
+    try {
+        const response = await fetch("/.netlify/functions/getusers");
+        if (!response.ok) { // NOT res.status >= 200 && res.status < 300
+            return { statusCode: response.status, body: response.statusText };
+        }
+        const data = await response.json();
+
+        return {
+            statusCode: 200,
+            body: data.value
+            // if you want to return whole json string
+            // headers: { 'Content-Type': 'application/json' },
+            // body: JSON.stringify(data)
+        };
+    } catch (err) {
+        console.log(err); // output to netlify function log
+        return {
+            statusCode: 500,
+            body: err.message // Could be a custom message or object i.e. JSON.stringify(err)
+        };
+    }
 }
 
 fetchUsers().then(data => {
